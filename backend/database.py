01232af12b709669d0bd6f85d7670b3626aedbef
KevinGuevara7 <gerayguevara@gmail.com>
2026-05-07 08:24:49 -0500

    add function get_db

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,27 @@
-# Importaciones
-from sqlalchemy import create_engine    # Conexión entre Python y la base de dtos
-from sqlalchemy.orm import sessionmaker, declarative_base   # Herramientas ORM de SQLAlchemy, sessionmaker crea sesiones de base de datos
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL")    # URL de la base de datos
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-        DATABASE_URL,
-        connect_args={"sslmode": "require"}
-    )    # Abre y administra conexiones, ejecuta consultas SQL
-
-# Se define como se crearán las sesiones de base de datos
-SessionLocal = sessionmaker(
-    autocommit=False,   # Los cambios no se guardan automáticamente
-    autoflush=False,    # Evita que SQLAlchemy envíe cambios automaticamente a la db sin que uno decida
-    bind=engine     # Conecta las sesiones con el engine que se creó antes
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
 )
 
-Base = declarative_base()   # Crea la clase base para todos los modelos de la base de datos
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
